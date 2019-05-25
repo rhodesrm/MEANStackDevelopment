3c5d9bfca2cc78234fc0f623fbfb13e367975c01
Robin R <rhodesr@bu.edu>
2019-05-25 11:55:41 -0400

    recommiting P1

--- PS1/P1.js
+++ PS1/P1.js
@@ -1,7 +1,20 @@
 const alphaOrder = (str) => {
+    arr = [];
     new_str = "";
-
+    for(i=0;i<str.length;i++)
+    {
+        arr[i] = str[i];
+        // if (arr[i] in ['!','.',',','?',';','1','2','3','4','5','6','7','8','9','0'])
+        // {
+        //     delete arr[i];
+        // }
+        if (['!','.',',','?',';','`','1','2','3','4','5','6','7','8','9','0'].includes(arr[i]))
+        {
+            delete arr[i];
+        }
+    }
+    arr.sort()
+    console.log(arr.toString());
 }
 
-
-console.log(a < b)
+alphaOrder('supercalifragilisticexpialidocious..');
